73c2da6acea4688526bde4ea4534f8e01c7c2ea0
maurovictor <mauro.victor5@gmail.com>
2017-08-09 13:12:45 -0300

    adding network software, samba and gigolo

--- post-instalation.sh
+++ post-instalation.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y vim git 
+sudo apt-get install -y vim git samba gigolo 
 
 sudo echo "[device] wifi.scan-rand-mac-address=no" >> /etc/NetworkManager/NetworkManager.conf
 sudo service network-manager restart
