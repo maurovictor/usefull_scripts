81f18309f2f8bbdce9fbcf7fa537d41fb47bae42
maurovictor <mauro.victor5@gmail.com>
2017-08-09 13:29:02 -0300

    adding comments

--- post-instalation.sh
+++ post-instalation.sh
@@ -1,4 +1,5 @@
-sudo apt-get install -y vim git samba gigolo 
+sudo apt-get install -y vim git samba gigolo smbclient 
 
+## Fixing Network wifi problem( ever changing mac address)
 sudo echo "[device] wifi.scan-rand-mac-address=no" >> /etc/NetworkManager/NetworkManager.conf
 sudo service network-manager restart
